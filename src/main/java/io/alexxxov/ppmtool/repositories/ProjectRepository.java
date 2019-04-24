c797ca4b317b78b615cf54924def8686230effe3
Alexander <aav94@yandex.ru>
2019-04-25 00:57:10 +0300

    added project repo

---
+++ src/main/java/io/alexxxov/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.alexxxov.ppmtool.repositories;
+
+import io.alexxxov.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Admin on 25.04.2019.
+ */
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
