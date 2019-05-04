de496c4e0c9c3aa75f2ce4285915dfda7ee87d1d
Alexander <aav94@yandex.ru>
2019-05-05 01:04:46 +0300

    created redux store and createProject axios

--- ppmtool-back/src/main/java/io/alexxxov/ppmtool/web/ProjectController.java
+++ ppmtool-back/src/main/java/io/alexxxov/ppmtool/web/ProjectController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,6 +16,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
--- ppmtool-react-client/src/components/Project/ProjectItem.js
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -14,7 +14,8 @@ class ProjectItem extends Component {
                       <p>Project to create a Kanban Board with Spring Boot and React</p>
                   </div>
                   <div className="col-md-4 d-none d-lg-block">
-                      <ul className="list-group">
+                      <ul className=
+                      "list-group">
                           <a href="#">
                               <li className="list-group-item board">
                                   <i className="fa fa-flag-checkered pr-1"> Project Board </i>
